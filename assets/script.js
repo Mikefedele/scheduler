aa3c8829833f2d2db4a668440ca2722ef2667448
Mike Fedele <mikefid11@gmail.com>
2022-03-24 10:02:23 -0400

    event target

--- assets/script.js
+++ assets/script.js
@@ -2,19 +2,28 @@ console.log($)
 var inputAreaEl = document.getElementsByClassName(".inputArea");
 var saveButtonEl = document.getElementsByClassName("saveButton");
 
+
+
+$(".container").on("click",".saveButton", function (event) {
+  console.log(event.target);
+  $(event.target);
+})
+
+// var inputEl =
+
 // inputAreaEl.append(inputAreaEl);
 // console.log(inputAreaEl);
  
 // // let inputAreaEl, value
 
 
+// i want to replace current list w new list or just add and male button be save
+// function savelist() {
+//   inputAreaEl.textContent = inputAreaEl;
+//   localStorage.setItem(inputAreaEl);
+// }
 
-function savelist() {
-  inputAreaEl.textContent = inputAreaEl;
-  localStorage.setItem(inputAreaEl);
-}
-
-saveButtonEl.on ("click", savelist());
+// saveButtonEl.on ("click", savelist());
 
 
 
