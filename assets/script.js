c8bfeee520b0e841be64c7da8becaa64f72e6ce5
Mike Fedele <mikefid11@gmail.com>
2022-03-23 22:13:33 -0400

    added on click

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,25 @@
 console.log($)
+var inputAreaEl = document.getElementsByClassName(".inputArea");
+var saveButtonEl = document.getElementsByClassName("saveButton");
+
+// inputAreaEl.append(inputAreaEl);
+// console.log(inputAreaEl);
+ 
+// // let inputAreaEl, value
+
+
+
+function savelist() {
+  inputAreaEl.textContent = inputAreaEl;
+  localStorage.setItem(inputAreaEl);
+}
+
+saveButtonEl.on ("click", savelist());
+
+
+
+
+// function getinput() {
+
+// }
+
